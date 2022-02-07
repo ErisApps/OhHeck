2b44a803081807443dd8d1637d2e93910bcd9a3b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-02-07 13:04:18 -0400

    Excessive parenting events

---
+++ OhHeck.Core/Analyzer/Lints/Animation/TooManyParentingTracks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OhHeck.Core.Helpers.Enumerable;
+using OhHeck.Core.Models.Beatmap;
+using OhHeck.Core.Models.ModData.Tracks;
+
+namespace OhHeck.Core.Analyzer.Lints.Animation;
+
+[BeatmapWarning("excessive-parenting")]
+public class TooManyParentingTracks : IBeatmapAnalyzer
+{
+	// grrr 2000 objects
+
+	// TODO: Inject?
+	private const int TRACK_PARENTING_EXCESS = 20;
+
+	public void Validate(Type fieldType, object? value, IWarningOutput warningOutput)
+	{
+		if (value is not List<BeatmapCustomEvent> events)
+		{
+			return;
+		}
+
+		var parentTrackCount = events.Count(e => e.Type == EventTypes.ASSIGN_TRACK_PARENT);
+
+		if (parentTrackCount > TRACK_PARENTING_EXCESS)
+		{
+			warningOutput.WriteWarning($"Excessive amounts of parent track events. Parenting is inefficient, consider using less. Found {parentTrackCount} > {TRACK_PARENTING_EXCESS}");
+		}
+	}
+}
