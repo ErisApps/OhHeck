7bbb1d04b679843d59777502036a182e05ff906e
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-02-07 10:33:03 -0400

    Regex testing no idea if this will work since it's not boost

---
+++ OhHeck.Core/Analyzer/Lints/EnvironmentRegex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OhHeck.Core.Helpers;
+using OhHeck.Core.Models.ModData.Chroma;
+using OhHeck.Core.Models.Structs;
+
+namespace OhHeck.Core.Analyzer.Lints;
+
+[BeatmapWarning("environment-regex")]
+public class EnvironmentRegex : IBeatmapAnalyzer {
+
+	public void Validate(Type fieldType, object? value, IWarningOutput warningOutput)
+	{
+		if (value is not EnvironmentEnhancement { LookupMethod: LookupMethod.Regex } environmentEnhancement)
+		{
+			return;
+		}
+
+		var regex = environmentEnhancement.Id;
+
+		if (!RegexBindings.IsRegexValid(regex, out var message))
+		{
+			warningOutput.WriteWarning(message);
+		}
+	}
+}
---
+++ OhHeck.Core/Helpers/RegexBindings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace OhHeck.Core.Helpers;
+
+public class RegexBindings
+{
+
+	public static bool IsRegexValid(string regexStr, [NotNullWhen(false)] out string? message)
+	{
+		try
+		{
+			Regex unused = new(regexStr, RegexOptions.CultureInvariant | RegexOptions.ECMAScript);
+		}
+		catch (Exception e)
+		{
+			message = e.Message;
+			return false;
+		}
+
+		message = null;
+		return true;
+	}
+
+}
