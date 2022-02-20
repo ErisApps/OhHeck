6920b51e83616e4a948da3fd1add2577cf61acdf
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-02-20 09:26:37 -0400

    Add EndsWith lookup lint

---
+++ OhHeck.Core/Analyzer/Lints/EndsWithRegexLookup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OhHeck.Core.Models.ModData.Chroma;
+
+namespace OhHeck.Core.Analyzer.Lints;
+
+[BeatmapWarning("ends-with-regex-lookup")]
+public class EndsWithRegexLookup : IBeatmapAnalyzer
+{
+	public void Validate(Type fieldType, object? value, IWarningOutput warningOutput)
+	{
+		if (value is not List<EnvironmentEnhancement> environmentEnhancements)
+		{
+			return;
+		}
+
+		foreach (var regex in
+		         from environmentEnhancement in environmentEnhancements
+		         where environmentEnhancement.LookupMethod is LookupMethod.Regex
+		         select environmentEnhancement.Id into regex
+		         where regex.EndsWith("$") select regex)
+		{
+			warningOutput.WriteWarning($"Using $ at the end of a regex lookup. Consider using EndsWith lookup instead. Regex: {regex}");
+		}
+	}
+}
