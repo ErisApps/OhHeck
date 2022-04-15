f2979a737e3c04faf1520444bf939ba6d0def7c3
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-04-15 16:05:23 -0400

    Fix logic

--- OhHeck.Core/Analyzer/Lints/BadFakeWall.cs
+++ OhHeck.Core/Analyzer/Lints/BadFakeWall.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using OhHeck.Core.Analyzer.Attributes;
-using OhHeck.Core.Models.beatmap.v3;
+using OhHeck.Core.Models.Beatmap;
 using OhHeck.Core.Models.Structs;
 
 namespace OhHeck.Core.Analyzer.Lints;
@@ -10,12 +10,10 @@ public class BadFakeWall : IFieldAnalyzer {
 
 	public void Validate(Type fieldType, object? value, IWarningOutput outerWarningOutput)
 	{
-		if (value is not ObstacleData obstacleData || obstacleData.ObstacleCustomData is null)
+		if (value is not ObstacleCustomData obstacleCustomData)
 		{
 			return;
 		}
-
-		var obstacleCustomData = obstacleData.ObstacleCustomData;
 
 		var fakeNullable = obstacleCustomData.Fake?.IsTrue();
 		var interactableNullable = obstacleCustomData.Cuttable?.IsTrue();
@@ -25,7 +23,7 @@ public class BadFakeWall : IFieldAnalyzer {
 
 		// if fake == true || interactable == false, fake wall
 		// if the counterpart field is not the opposite value, unoptimized wall
-		if ((fakeNullable is not null || interactableNullable is not null) && fake != interactable)
+		if ((fakeNullable is null && interactableNullable is null) || fake != interactable)
 		{
 			return;
 		}
